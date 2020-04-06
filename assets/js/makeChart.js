60009d55d5025fd2a739ec691bcae174c87627c8
Jessie Wright <jessie.wright@gmail.com>
2020-04-06 04:04:35 +0000

    handle array of values

--- assets/js/makeChart.js
+++ assets/js/makeChart.js
@@ -1,8 +1,13 @@
-const makeChart = function(numBars) {
+const makeChart = function(data) {
+  let maxVal = Math.max(...data);
 
-  for(let i = 0; i < numBars; i++) {
+
+  for(let i = 0; i < data.length; i++) {
     let node = document.createElement("div");
     node.classList.add("t2");
+    let heightPercentage = data[i] / maxVal * 100;
+    console.log(heightPercentage);
+    node.style.height =  String(heightPercentage) + '%';
     document.getElementById("chart").appendChild(node);
   }
 
